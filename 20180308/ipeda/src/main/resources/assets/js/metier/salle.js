03f58fbb06bde147dbab86391ab6ca0acd6e4529
Guillaume <lefetz@gmail.com>
2018-05-20 00:50:15 +0200

    salles jquery (pas d'appel ajax avant submit pour modales)

--- 20180308/ipeda/src/main/resources/assets/js/metier/salle.js
+++ 20180308/ipeda/src/main/resources/assets/js/metier/salle.js
@@ -1,23 +1,41 @@
-function openSalleModal() {
-	$("#modal-container").modal("show");
+
+function openSalleModalCreate(modalId) {
+	getModalContainer(modalId).modal("show");
 }
 
-function openSalleModalUpdate(id) {
+function openSalleModalUpdate(modalId, id) {
+	var container = getModalContainer(modalId);
+	var tr = findTrFromIdInDatatable(id);
+	if(tr != false) {
+		container.find("#id").val(tr.find("td:eq(0)").text());
+		container.find("#libelle").val(tr.find("td:eq(1)").text());
+		container.modal("show");
+	}
+}
 
-	$.ajax({
-		url: "/rest/salles/update",
-		data: { id : id },
-		success: function(data) {
-			$("#id").val(data.id);
-			$("#libelle").val(data.libelle);
-			$("#modal-container").modal("show");
+function openSalleModalDelete(modalId, id) {
+	var container = getModalContainer(modalId);
+	var tr = findTrFromIdInDatatable(id);
+	if(tr != false) {
+		container.find("#id").val(tr.find("td:eq(0)").text());
+		var message = container.find("#message").html();
+		container.find("#message").html(message.replace("{0}", tr.find("td:eq(1)").text()));
+		container.modal("show");
+	}
+}
+
+function findTrFromIdInDatatable(id) {
+	var result;
+	$("table#example tbody tr").each(function(index ) {
+		var tdId = $(this).find("td:eq(0)").text();
+		if(tdId == id) {
+			result = $(this);
+			return false;
 		}
 	});
-	
+	return result;
 }
 
-function openSalleModalDelete(id) {
-
-
-	
+function getModalContainer(modalId) {
+	return $("#modal-container-" + modalId);
 }
